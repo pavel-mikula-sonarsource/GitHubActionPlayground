e3293c7cec971ef63abfc414df96a3c763d70e39
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-10-07 13:04:08 +0200

    Assignee prototype

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const github = require('@actions/github');
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
+    const token = core.getInput('token');
+    const issue_number = 1;//core.getInput('issue_number');
+    const assignee_id = core.getInput('assignee_id');
+    const octokit = github.getOctokit(token)
+    console.log(`assignee_id: ${assignee_id}!`);
+
+    //octokit.issues.removeAssignees({ repo: github.context, issue_number: issue_number, assignees: [] });
+    octokit.issues.addAssignees({ repo: github.context, issue_number: issue_number, assignees =[assignee_id] });
+
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
+
 } catch (error) {
     core.setFailed(error.message);
 }
+
